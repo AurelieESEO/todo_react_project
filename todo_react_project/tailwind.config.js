084fab3ca025e8456ea799e4b0ab626e2260c2e1
achamouleau <aurelie.chamouleau@reseau.eseo.fr>
2023-11-13 11:55:03 +0100

    tailwind.config.js with new theme

--- todo_react_project/tailwind.config.js
+++ todo_react_project/tailwind.config.js
@@ -5,7 +5,30 @@ export default {
       "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            display: ['IBM Plex Mono', 'Menlo', 'monospace'],
+            body: ['IBM Plex Mono', 'Menlo', 'monospace'],
+        },
+        colors: {
+            primary: {
+                50: '#F6F3FE',
+                100: '#E2D6FF',
+                200: '#CDC7FF',
+                300: '#C3C0FF',
+                400: '#B8B8FF',
+            },
+            accent: {
+                500: "#150833",
+            },
+            playful: {
+                50: '#E1FFBD',
+                100: '#FFEDBD',
+                200: '#D6E4FF',
+                300: '#FDD6FF',
+            },
+        },
+    },
   },
   plugins: [],
 }
