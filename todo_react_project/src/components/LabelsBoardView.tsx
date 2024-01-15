831db0ef5e02d9f54e44b7264455714f3b7a1348
achamouleau <aurelie.chamouleau@reseau.eseo.fr>
2024-01-15 12:02:45 +0100

    Possibility to drag and drop task items in the kanban page and it changes it's status
    TODO:
    - UI of task item + kanban
    - when status = done -> ui of a done task item in the list (pink + stroke through)

--- todo_react_project/src/components/LabelsBoardView.tsx
+++ todo_react_project/src/components/LabelsBoardView.tsx
@@ -1,28 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
 import Task from "../model/Task.tsx";
 import Tag from "../model/Tag.tsx";
+import TaskItem from "./TaskItem.tsx";
 
 type LabelsBoardViewProps = {
   tagsPossibles: Tag[];
   tasks: Task[];
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const LabelsBoardView: React.FC<LabelsBoardViewProps> = ({tagsPossibles, tasks}) => {
-  //const tagsPossibles = ["Cuisine", "Ménage", "Travail", "Famille",
-  // "Finances"];
+const LabelsBoardView: React.FC<LabelsBoardViewProps> = ({tasks }) => {
+  const [draggedTaskId, setDraggedTaskId] = useState<number | null>(null);
 
-  console.log(tagsPossibles);
-  console.log(tasks);
+  const handleDragDrop = (draggedTaskId: number | null, newStatus: string) => {
+    console.log(draggedTaskId);
+    console.log(`Task ${draggedTaskId} dropped into ${newStatus} column`);
+    tasks.map((task) => {
+      if (task.id === draggedTaskId) {
+        task.status = newStatus;
+      }
+    });
+  };
 
   return (
-      <div className="flex flex-col w-full lg:flex-row">
+      <div className="p-4 flex flex-col gap-4 w-full h-full lg:flex-row">
         <div
-            className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content
+            className="flex-grow p-4 h-full card bg-secondary/20 overflow-y-auto"
+            onDragOver={(event) => {
+              event.preventDefault();
+            }}
+            onDrop={() => {
+              handleDragDrop(draggedTaskId, 'Not Started');
+              setDraggedTaskId(null);
+            }}
+        >
+          Not Started
+          {tasks.map((task) => (
+              task.status === "Not Started" && (
+                  <TaskItem
+                      key={task.id}
+                      task={task}
+                      isInStatusBoard={true}
+                      onDragStart={(taskId) => {
+                        setDraggedTaskId(taskId);
+                      }}
+                      onDragDrop={(taskId) => {
+                        setDraggedTaskId(taskId);
+                      }}
+                  />
+              )
+          ))}
         </div>
-        <div className="divider lg:divider-horizontal"></div>
         <div
-            className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content
+            className="flex-grow p-4 h-full card bg-accent/20 overflow-y-auto"
+            onDragOver={(event) => {
+              event.preventDefault();
+            }}
+            onDrop={() => {
+              handleDragDrop(draggedTaskId, 'In Waiting');
+              setDraggedTaskId(null);
+            }}
+        >
+          In Waiting
+          {tasks.map((task) => (
+              task.status === "In Waiting" && (
+                  <TaskItem
+                      key={task.id}
+                      task={task}
+                      isInStatusBoard={true}
+                      onDragStart={(taskId) => {
+                        setDraggedTaskId(taskId);
+                      }}
+                      onDragDrop={(taskId) => {
+                        setDraggedTaskId(taskId);
+                      }}
+                  />
+              )
+          ))}
+        </div>
+        <div
+            className="flex-grow p-4 h-full card bg-primary/20 overflow-y-auto"
+            onDragOver={(event) => {
+              event.preventDefault();
+            }}
+            onDrop={() => {
+              handleDragDrop(draggedTaskId, 'In Progress');
+              setDraggedTaskId(null);
+            }}
+        >
+          In Progress
+          {tasks.map((task) => (
+              task.status === "In Progress" && (
+                  <TaskItem
+                      key={task.id}
+                      task={task}
+                      isInStatusBoard={true}
+                      onDragStart={(taskId) => {
+                        setDraggedTaskId(taskId);
+                      }}
+                      onDragDrop={(taskId) => {
+                        setDraggedTaskId(taskId);
+                      }}
+                  />
+              )
+          ))}
+        </div>
+        <div
+            className="flex-grow p-4 h-full card bg-success/20 overflow-y-auto"
+            onDragOver={(event) => {
+              event.preventDefault();
+            }}
+            onDrop={() => {
+              handleDragDrop(draggedTaskId, 'Done');
+              setDraggedTaskId(null);
+            }}
+        >
+          Done
+          {tasks.map((task) => (
+              task.status === "Done" && (
+                  <TaskItem
+                      key={task.id}
+                      task={task}
+                      isInStatusBoard={true}
+                      onDragStart={(taskId) => {
+                        setDraggedTaskId(taskId);
+                      }}
+                      onDragDrop={(taskId) => {
+                        setDraggedTaskId(taskId);
+                      }}
+                  />
+              )
+          ))}
         </div>
       </div>
   );
